5a8ba904677964902db0a1eb26b9d5ed2b09c726
dennys2001 <dennys2001@gmail.com>
2023-08-21 08:01:15 -0300

    tudo2

---
+++ server/index.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//ROTAS//
+
+//CRIAR FUNCIONÁRIO
+app.post("/create", async (req, res) => {
+    try {
+        const { level, nome, leaderId, cargo } = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO midia.ORGCHART (level, nome, lider_id, cargo) VALUES($1,$2,$3,$4) RETURNING *", 
+            [level, nome, leaderId, cargo]
+        );
+
+        res.json(newTodo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//BUSCAR TODOS OS FUNCIONÁRIOS
+
+app.get("/allemployees", async (req, res) => {
+    try {
+        
+        const allEmployees = await pool.query(
+            "SELECT * FROM midia.ORGCHART", 
+            
+        );
+
+        res.json(allEmployees.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//APAGAR FUNCIONÁRIO
+
+app.delete("/create/:id", async (req, res) => {
+    try {
+        
+        const { id } = req.params;
+        const delTodo = await pool.query(
+            "delete from midia.ORGCHART where id = ($1)", 
+            [id]
+        );
+            
+        res.json("Employee was deleted");
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//ATUALIZAR CARGO FUNCIONÁRIO
+
+app.put("/create/:id", async (req, res) => {
+    try {
+        
+        const { cargo } = req.body;
+        const { id } = req.params;
+        const updtTodos = await pool.query(
+            "UPDATE midia.ORGCHART SET cargo = ($1) where id = ($2)", 
+            [cargo, id]
+        );
+                //console.log(updtTodos);
+        res.json("Cargo was Updated");
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//BUSCAR LIDER E ESTRUTURA
+
+app.get("/allemployees/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const allEstrutura = await pool.query(
+            "SELECT * FROM midia.ORGCHART where id = ($1) or lider_id = ($1) or id_sub_estrutura = 1007 order by level", 
+            [id]
+        );
+
+        res.json(allEstrutura.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+
+
+
+
+app.listen(5000, () => {
+    console.log("server has started on port 5000")
+});
