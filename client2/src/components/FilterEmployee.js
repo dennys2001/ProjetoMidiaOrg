011e7e0dbdcc66ae751ec0e4a47c23bafe20571b
Denis Gato <dennys2001@gmail.com>
2023-08-29 16:53:40 -0300

    vixi

--- client2/src/components/FilterEmployee.js
+++ client2/src/components/FilterEmployee.js
@@ -74,7 +74,7 @@ return  <Fragment>
 
 
 <div class="modal" id="myModal">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
         <h4 class="modal-title">Estrutura do Time</h4>
