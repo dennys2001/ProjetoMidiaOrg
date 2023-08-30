24a4b7883571cb54f30274c6e3a782f94d3cadd7
Denis Gato <dennys2001@gmail.com>
2023-08-30 13:50:21 -0300

    newFront

---
+++ client3/client3/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Fragment } from 'react';
+
+
+function App() {
+  return (
+   <Fragment>
+    <h1>Oi Pessoal</h1>
+   </Fragment>
+  );
+}
+
+export default App;
