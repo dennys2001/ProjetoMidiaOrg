7d23f176f459310102ce736940eb9a6e44934e85
dennys2001 <dennys2001@gmail.com>
2023-09-12 12:31:05 -0300

    roundedImage

--- client3/client3/src/components/TeamCards.js
+++ client3/client3/src/components/TeamCards.js
@@ -6,13 +6,16 @@ const TeamCards = () => {
     <Fragment>
 
         <div class="container p-5 my-5 bg-dark text-white">
-            <h1>Responsive Columns</h1>
-            <p>Resize the browser window to see the effect.</p>
-            <p>The columns will automatically stack on top of each other when the screen is less than 576px wide.</p>
+            <h1>Teste Render Card</h1>
+            <p>Só falta Fazer Dinâmico</p>
+            
             <div class="row">
                 <div class="col-sm-3 p-3 bg-white text-black">
                     <div class="card" >
                         <img class="card-img-top" src={image1} alt="Card image" />
+
+                        
+
                         <div class="card-body">
                         <h4 class="card-title">Beka</h4>
                         <p class="card-text">Gerente da Porra Toda</p>
