ec683f08f73545c00fe7c16d47c71b6f27c50722
Denis Vedan <denis.vedan@navegg.com.br>
2023-09-13 17:32:25 -0300

    Dynamic_cards

--- client3/client3/src/components/TeamCards.js
+++ client3/client3/src/components/TeamCards.js
@@ -1,42 +1,55 @@
-import React, { Fragment } from "react";
-import image1 from "./img/img_gerente.png"
-const TeamCards = () => {
-   
+import React, { Fragment, useState } from "react";
+import image1 from "./img/logo192.png"
+
+
+
+
+const TeamCards = ({ diretoria }) => {
+    const [filhos, setFilhos] = useState([]);
+ 
+
+    const getFilhos = async id => {
+         console.log("clicou nos filhos de", id);
+         try {
+ 
+             const response = await fetch(`http://localhost:5000/allemployees/subestrutura/${id}`)
+             const jsonData = await response.json()
+ 
+             setFilhos(jsonData);
+         } catch (err) {
+             console.error(err.message);
+         }
+     }
+
     return (
     <Fragment>
 
         <div class="container p-5 my-5 bg-dark text-white">
-            <h1>Teste Render Card</h1>
-            <p>Só falta Fazer Dinâmico</p>
+            <h1>Conheça Nosso Time</h1>
+            <p>Agora só faltam as fotos</p>
             
             <div class="row">
-                <div class="col-sm-3 p-3 bg-white text-black">
-                    <div class="card" >
-                        <img class="card-img-top rounded-circle" src={image1} alt="Card image" />
-
-                        
+             {diretoria.map(diretor =>  (
+                <div class="col-sm-3 p-3 bg-white text-black" key={diretor.id}>
+                   
+                    <div class="card align-center"
+                    onClick={() => getFilhos(diretor.id)} >
+                        <img class="card-img-top rounded-circle" src={ image1 } alt="Card image" />
 
                         <div class="card-body">
-                        <h4 class="card-title">Beka</h4>
-                        <p class="card-text">Gerente da Porra Toda</p>
+                        <h4 class="card-title">{diretor.nome}</h4>
+                        <p class="card-text">{diretor.cargo}</p>
                         </div>
                     </div>
-                
+                   
                 </div>
 
-                <div class="col-sm-3 p-3 bg-warning text-white">
-                    <div class="card" >
-                        <img class="card-img-top rounded-circle" src={image1} alt="Card image" />
-                        <div class="card-body">
-                        <h4 class="card-title">Beka</h4>
-                        <p class="card-text">Gerente da Porra Toda</p>
-                        </div>
-                    </div>
-                </div>
+))}
+
+
+             
                 
-                
-                <div class="col-sm-3 p-3 bg-primary text-white">.col</div>
-                <div class="col-sm-3 p-3 bg-white text-dark">.col</div>
+          
             </div>
         </div> 
 
